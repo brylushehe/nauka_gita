a050735edae084a66757d750df038a676ff22f96
kuba <vdi-terminal@EDU-WSB.local>
2025-03-08 18:00:53 +0100

    srednia wazona

--- avg.php
+++ avg.php
@@ -1,6 +1,14 @@
 <?php
 $a = 10; // przykładowa wartość dla a
 $b = 20; // przykładowa wartość dla b
-$średnia = ($a + $b) / 2; // obliczanie średniej
-echo $średnia; // wyświetlenie wyniku
+
+// Średnia arytmetyczna
+$średnia = ($a + $b) / 2;
+echo "Średnia arytmetyczna: " . $średnia . "\n";
+
+// Średnia ważona (przykładowe wagi: waga_a = 0.6, waga_b = 0.4)
+$waga_a = 0.6;
+$waga_b = 0.4;
+$średnia_ważona = ($a * $waga_a + $b * $waga_b) / ($waga_a + $waga_b);
+echo "Średnia ważona: " . $średnia_ważona . "\n";
 ?>
