d846c630f2d99dab72b4ba3b0ec87129364bb80c
kuba <vdi-terminal@EDU-WSB.local>
2025-03-08 18:18:44 +0100

    wagi

--- avg.php
+++ avg.php
@@ -3,4 +3,5 @@ $a = 10; // przykładowa wartość dla a
 $b = 20; // przykładowa wartość dla b
 $średnia = ($a + $b) / 2; // obliczanie średniej
 echo $średnia; // wyświetlenie wyniku
+//dodaje wagi
 ?>
